eeb8acace0261c07a125ef9890a49e3be337005d
Matthew Nguyen <contact@n-studio.fr>
2018-10-25 05:32:55 +0200

    Added controller for letsencrypt

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,12 @@
 class StaticController < ApplicationController
   def about
   end
+  
+  def letsencrypt
+    if Rails.application.credentials[Rails.env.to_sym][:letsencrypt_acme_challenge].keys.map(&:to_s).include?(params[:id])
+      render plain: params[:id] + "." + Rails.application.credentials[Rails.env.to_sym][:letsencrypt_acme_challenge][params[:id].to_sym]
+    else
+      render plain: nil
+    end
+  end
 end
